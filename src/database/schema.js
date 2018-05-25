e3c1212897d90f6620563df38d9aeeab0e43d0c5
Shannon <schristinep@gmail.com>
2018-05-25 01:12:43 -0500

    moves schema outside of connection cb because it turns out restarting the server fixed everything

--- src/database/schema.js
+++ src/database/schema.js
@@ -10,6 +10,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.on('connected', () => { console.log('connected'); });
 db.once('open', () => {
+});
 
   const articleSchema = mongoose.Schema({
     title: String,
@@ -19,23 +20,22 @@ db.once('open', () => {
     lastUpdate: { type: Date, default: Date.now },
     tags: Array,
   });
-  
+
   // add methods here, before compiling with model
-  
+
   const Article = mongoose.model('Article', articleSchema);
 
-});
+  const getAllArticlesByDate = function (cb) {
+    Article.find({}).sort('dateCreated').exec((err, results) => {
+      console.log('error', err, 'results', results);
+      cb(err, results);
+    });
+  };
 
 
 // instance: const article = new Article({ title: 'some string', author: 'some name string', etc. })
 // could add method to find same author, probably same tags?
 // maybe do a separate method for both of those tho
 
-const getAllArticlesByDate = function (cb) {
-  Article.find({}).sort('dateCreated').exec((err, results) => {
-    console.log('error', err, 'results', results);
-    cb(err, results);
-  });
-};
 
 module.exports = getAllArticlesByDate;
