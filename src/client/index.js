4f7bc2c3d3b074d96e47f1eb66ae399f203e6bee
Shannon <schristinep@gmail.com>
2018-05-24 19:17:20 -0500

    more cleanup for linter (npm i y'all)

--- src/client/index.js
+++ src/client/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+/* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(<App />, document.getElementById('app'));
+/* eslint-enable jsx-filename-extension */
