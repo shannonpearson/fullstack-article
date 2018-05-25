c854ecbe55302015b76454cf7a72557694a61c2c
Shannon <schristinep@gmail.com>
2018-05-25 00:07:49 -0500

    adds article body and tags, formats

--- src/client/App.js
+++ src/client/App.js
@@ -28,7 +28,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        smh
         <ArticleList articles={sampleData} />
       </div>
     )
--- src/client/components/ArticlePreview.jsx
+++ src/client/components/ArticlePreview.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Panel, Collapse, Label } from 'react-bootstrap';
 
 const { Component } = React;
-const { Header, Brand } = Navbar;
+const {
+  Heading, Title, Body, Footer,
+} = Panel;
 
 class ArticlePreview extends Component {
   constructor(props) {
@@ -17,13 +19,16 @@ class ArticlePreview extends Component {
   render() {
     const { article } = this.props;
     return (
-        <Navbar>
-            <Header>
-                <Brand>
-                    holla
-                </Brand>
-            </Header>
-        </Navbar>
+      <Panel bsStyle="info">
+        <Heading>
+          <Title componentClass="h3"> { article.title } </Title>
+        </Heading>
+        <Body> { article.body} </Body>
+        <Footer>
+          { article.tags.map(tag => (<Label key={tag} bsStyle="info" style={{ marginRight: '5' }}> {tag} </Label>))
+            }
+        </Footer>
+      </Panel>
     );
     // (
     //   <Card title={article.title}>
