1f7a85c7e28112145df6150e1568987843ce0db4
Shannon <schristinep@gmail.com>
2018-05-24 20:34:47 -0500

    test change

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,13 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
 
 class App extends Component {
 
   render() {
-
     return (
       <div>
-        Hello world!
+        hi
       </div>
     )
   }
